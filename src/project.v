5d90b3688d0deda9285a3dd7f5b6af58ae06f2ea
Mike Goelzer <mikegoelzer@goelzer.org>
2024-11-08 03:23:15 +0000

    added 7-segment and cocotb tests

--- src/project.v
+++ src/project.v
@@ -6,10 +6,11 @@
 `default_nettype none
 
 module tt_um_mikegoelzer_7segmentbyte (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
+    input  wire [7:0] ui_in,    // byte to display
+    output wire [7:0] uo_out,   // [0] = 7-segment a, [1] = 7-segment b, etc.
+                                // [7] = cathode control (0=right, 1=left)
+    input  wire [7:0] uio_in,   // [0] = write enable, rest unused
+    output wire [7:0] uio_out,  // (not used)
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -17,11 +18,26 @@ module tt_um_mikegoelzer_7segmentbyte (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = 0;  // not used
+  assign uio_oe  = 0;  // only uio[0] is used (as a WE input, active high)
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
+
+  seven_seg seven_seg(
+    .clk     (clk      ),
+    .rst_n   (rst_n    ),
+    .disp_off(1'b0     ),
+    .valid   (uio_in[0]),
+    .val     (ui_in    ),
+    .aa      (uo_out[0]),
+    .ab      (uo_out[1]),
+    .ac      (uo_out[2]),
+    .ad      (uo_out[3]),
+    .ae      (uo_out[4]),
+    .af      (uo_out[5]),
+    .ag      (uo_out[6]),
+    .cathode (uo_out[7])
+  );
 
 endmodule
